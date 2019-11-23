6d91e49b2ff8dc6c6edd1f68e9c066a4a704a235
BePsvPT <bepsvpt@protonmail.com>
2019-11-23 21:38:45 +0800

    use env port

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ require('dotenv').config({});
 require('console-stamp')(console, 'mm-dd HH:MM:ss.l');
 
 const httpServer = require('./http-server');
-const isRoot = require('is-root');
 const turn = require('./turn-server');
 
 turn.start();
 
 require('socket.io')(httpServer).on('connect', require('./stream-server'));
 
-httpServer.listen(process.env.SECURE === 'true' ? (isRoot() ? 443 : 3000) : (isRoot() ? 80 : 3000));
+httpServer.listen(process.env.PORT);
