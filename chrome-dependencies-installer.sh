7c4135240356407b3a52eb1867e40a05a19ca2bb
BePsvPT <bepsvpt@protonmail.com>
2019-11-23 18:33:22 +0800

    add chrome dependencies install script

---
+++ chrome-dependencies-installer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+apt install xvfb \
+libxcomposite1 \
+libxcursor1 \
+libxss1 \
+libxrandr2 \
+libatk1.0-0 \
+libatk-bridge2.0-0 \
+libpangocairo-1.0-0 \
+libgtk-3-0 \
+libnss3 \
+libasound2 \
+fontconfig \
+fonts-ipafont-gothic \
+fonts-wqy-zenhei \
+fonts-thai-tlwg \
+fonts-kacst \
+fonts-symbola \
+fonts-noto \
+fonts-freefont-ttf
