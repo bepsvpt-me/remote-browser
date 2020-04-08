79a5d2264a1378f9d4d6a61f60081a51e66e4452
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-04-08 20:12:43 +0800

    refactor webrtc ice servers fetcher

---
+++ ice-servers/index.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto');
+const sServer = process.env.STUN_SERVER;
+const tServer = process.env.TURN_SERVER;
+const key = process.env.TURN_KEY;
+
+const expiration = () => {
+  const microsecond = +new Date;
+
+  const timestamp = Math.trunc(microsecond / 1000);
+
+  return timestamp + (60 * 60 * 1); // one hour
+}
+
+const random = () => {
+  return crypto.randomBytes(8)
+    .toString('hex');
+}
+
+const hmac = (data) => {
+  return crypto.createHmac('sha1', key)
+    .update(data)
+    .digest('base64');
+}
+
+module.exports = () => {
+  const servers = [];
+
+  if (sServer) {
+    servers.push({ urls: `stun:${sServer}` })
+  }
+
+  if (tServer && key) {
+    const username = `${expiration()}:${random()}`;
+
+    servers.push({
+      urls: `turn:${tServer}`,
+      username: username,
+      credential: hmac(username)
+    })
+  }
+
+  return servers;
+}
