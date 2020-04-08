2260fe15ada91268ad75282a8d52de49756b64f4
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-04-08 20:17:50 +0800

    show listening message

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config({});
 require('console-stamp')(console, 'mm-dd HH:MM:ss.l');
 
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || '3000';
 const httpServer = require('./http-server');
 
 require('socket.io')(httpServer, { cookie: false })
   .on('connect', require('./stream-server'));
 
-httpServer.listen(process.env.PORT, process.env.HOST);
+httpServer.listen(PORT, HOST);
+
+console.log(`Listen on http://${HOST}:${PORT}`);
