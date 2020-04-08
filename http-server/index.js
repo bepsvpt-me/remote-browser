c22d3a377ed0ac50249b7dd729094f98e03f68b3
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-04-08 20:15:07 +0800

    remove ssl support (use reverse proxy)

--- http-server/index.js
+++ http-server/index.js
@@ -1,15 +1,3 @@
 const app = require('./client');
-const fs = require('fs');
-const path = require('path');
 
-if (process.env.SECURE !== 'true') {
-  module.exports = require('http').createServer(app);
-} else {
-  const opts = {
-    key: fs.readFileSync(path.join(__dirname, 'certs', 'privkey.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'certs', 'fullchain.pem'))
-  };
-
-  module.exports = require('https').createServer(opts, app);
-}
-
+module.exports = require('http').createServer(app);
