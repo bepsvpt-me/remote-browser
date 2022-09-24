94edb2268a9db412b18fd16f15ad386a0c240a88
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2022-09-24 13:06:17 +0800

    fix start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-xvfb-run -a --server-args="-screen 0 1x1x24 -ac -nolisten tcp +extension RANDR" node index.js
+xvfb-run -a --server-args="-screen 0 1x1x24 -ac -nolisten tcp +extension RANDR" node --require dotenv/config index.js
